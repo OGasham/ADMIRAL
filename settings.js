ad6ace29612f1af3546028b631b1abdff72b8c1f
OGasham <yusifqesem.coc@gmail.com>
2025-09-06 01:29:23 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "ADMIRAL BOT",
   botOwner: 'ADMIRAL™', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: 'Your Number', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
